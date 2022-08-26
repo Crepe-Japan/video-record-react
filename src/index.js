09741a6adf5b2c32ec06e284a8a87d09ab990218
Pyae Phyo Kyaw <pyaephyokyaw.work@gmail.com>
2022-08-26 16:35:59 +0900

    init: video js adding

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const videoJsOptions = {
+  controls: true,
+  bigPlayButton: false,
+  width: 320,
+  height: 240,
+  fluid: false,
+  plugins: {
+    /*
+    // wavesurfer section is only needed when recording audio-only
+    wavesurfer: {
+        backend: 'WebAudio',
+        waveColor: '#36393b',
+        progressColor: 'black',
+        debug: true,
+        cursorWidth: 1,
+        msDisplayMax: 20,
+        hideScrollbar: true,
+        displayMilliseconds: true,
+        plugins: [
+            // enable microphone plugin
+            WaveSurfer.microphone.create({
+                bufferSize: 4096,
+                numberOfInputChannels: 1,
+                numberOfOutputChannels: 1,
+                constraints: {
+                    video: false,
+                    audio: true
+                }
+            })
+        ]
+    },
+    */
+    record: {
+      audio: true,
+      video: true,
+      maxLength: 10,
+      debug: true
+    }
+  }
+};
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App {...videoJsOptions} />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
