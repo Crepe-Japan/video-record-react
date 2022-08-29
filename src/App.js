a798bed08e689e37778212cbe61917838f83de49
Pyae Phyo Kyaw <pyaephyokyaw.work@gmail.com>
2022-08-29 14:20:49 +0900

    add: video input changing

--- src/App.js
+++ src/App.js
@@ -28,7 +28,8 @@ export default function App({ ...options }) {
 
 
   useEffect(() => {
-
+    var devices, deviceId;
+    var inputSection = document.getElementsByClassName('inputSelector')[0];
     // Anything in here is fired on component mount.
     const player = videojs('myVideo', options, function () {
       // print version information at startup
@@ -39,11 +40,6 @@ export default function App({ ...options }) {
       console.log("videojs-record is ready!");
     });
     /*  setPlayer(vidPlayer) */
-
-
-    player.on('deviceReady', () => {
-      console.log('device is ready!');
-    });
 
     // user clicked the record button and started recording
     player.on('startRecord', () => {
@@ -73,6 +69,70 @@ export default function App({ ...options }) {
       console.error('device error:', this.player.deviceErrorCode);
     });
 
+    // enumerate devices once
+    player.one('deviceReady', function () {
+      player.record().enumerateDevices();
+    });
+
+    player.on('enumerateReady', function () {
+      devices = player.record().devices;
+
+      // handle selection changes
+      var inputSelector = document.getElementById('selector');
+      inputSelector.addEventListener('change', changeVideoInput);
+
+      // populate select options
+      var deviceInfo, option, i;
+      for (i = 0; i !== devices.length; ++i) {
+        deviceInfo = devices[i];
+        option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === 'videoinput') {
+          console.info('Found video input device: ', deviceInfo.label);
+          option.text = deviceInfo.label || 'input device ' +
+            (inputSelector.length + 1);
+          inputSelector.appendChild(option);
+        }
+      }
+
+      if (inputSelector.length == 0) {
+        // no output devices found, disable select
+        option = document.createElement('option');
+        option.text = 'No video input devices found';
+        option.value = undefined;
+        inputSelector.appendChild(option);
+        inputSelector.disabled = true;
+        console.warn(option.text);
+      } else {
+        console.info('Total video input devices found:', inputSelector.length);
+      }
+
+      // show input selector section
+      inputSection.style.display = 'block';
+    });
+
+    function changeVideoInput(event) {
+      var label = event.target.options[event.target.selectedIndex].text;
+      deviceId = event.target.value;
+
+      try {
+        // change video input device
+        player.record().setVideoInput(deviceId);
+
+        console.log("Changed video input to '" + label + "' (deviceId: " +
+          deviceId + ")");
+      } catch (error) {
+        console.warn(error);
+
+        // jump back to first output device in the list as it's the default
+        event.target.selectedIndex = 0;
+      }
+    }
+
+    // error handling
+    player.on('enumerateError', function () {
+      console.warn('enumerate error:', player.enumerateErrorCode);
+    });
 
     return () => {
       // Anything in here is fired on component unmount.
@@ -85,7 +145,12 @@ export default function App({ ...options }) {
   return (
     <div data-vjs-player>
       <video id="myVideo" className="video-js vjs-default-skin" playsInline></video>
+      <div className="inputSelector">
+        <label>Select video input: </label>
+        <select id="selector"></select>
+      </div>
     </div>
+
   );
 
 }
