8c5035490a349dba2eaff972530846cf43256c7b
Pyae Phyo Kyaw <pyaephyokyaw.work@gmail.com>
2022-09-05 18:54:40 +0900

    refactor: remove unused var

--- src/canvas-stream.js
+++ src/canvas-stream.js
@@ -34,9 +34,9 @@ export const canvasStreamer = {
         let l = frame.data.length / 4;
 
         for (let i = 0; i < l; i++) {
-            let r = frame.data[i * 4 + 0];
-            let g = frame.data[i * 4 + 1];
-            let b = frame.data[i * 4 + 2];
+            /*   let r = frame.data[i * 4 + 0];
+              let g = frame.data[i * 4 + 1];
+              let b = frame.data[i * 4 + 2]; */
 
             frame.data[i * 4 + 1] += 100
             frame.data[i * 4 + 2] += 20
