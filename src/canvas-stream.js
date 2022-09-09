e13d675720d5c40e7e59c350985606584b0754a7
Pyae Phyo Kyaw <pyaephyokyaw.work@gmail.com>
2022-09-09 14:22:29 +0900

    test: back to orignal position

--- src/canvas-stream.js
+++ src/canvas-stream.js
@@ -47,7 +47,7 @@ export const canvasStreamer = {
 
         this.ctx1.drawImage(this.cameraVideo, 0, 0, this.width, this.height);
         /*         const newWidth = this.playerVideo.videoWidth * this.height / this.playerVideo.videoHeight */
-        this.ctx1.drawImage(this.playerVideo, this.width / 2 - this.playerVideoCalcWidth / 2, 0, this.playerVideoCalcWidth, this.height)
+        this.ctx1.drawImage(this.playerVideo, 0, 0, this.width, this.height)
         /*         let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
                 let l = frame.data.length / 4;
         
